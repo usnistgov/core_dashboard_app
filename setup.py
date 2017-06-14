56b743be7f8b2775d2852a98200f656d6ee9d21a
Adrien Catel <adrien.catel@nist.gov>
2017-06-14 16:09:52 -0400

    pypi settings + alpha2
    Change-Id: I0153eaa62d47d64e6cd7ef21e11f36b691c6bc76

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_dashboard_app",
-    version="1.0.0-alpha1",
+    version="1.0.0-alpha2",
     description=("Core dashboard Django package",),
-    author="no_author",
-    author_email="contact@example.com",
-
+    author="NIST IT Lab",
+    author_email="itl_inquiries@nist.gov",
     packages=find_packages(),
     include_package_data=True,
     install_requires=required,
