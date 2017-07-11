f76e55c8b7db32edbd5c131d4e1400272a87721f
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2017-07-11 13:35:36 -0400

    FIX Delete/Restore other's templates reloads the page on Mine
    Change-Id: I0e27c319809d5e18a04e9e88e99ba0acf1b39454

--- core_dashboard_app/static/core_dashboard_app/user/js/init.js
+++ core_dashboard_app/static/core_dashboard_app/user/js/init.js
@@ -3,5 +3,16 @@ $(document).ready(function(){
         $(this).closest("." + $(this).attr("data-hide")).hide(200);
     });
 
+    // Change hash for page-reload 
+    $('.nav-tabs a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+    });
+
+    // Use the hash to select the correct tab (if available)
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+    }
+
     init();
 });
