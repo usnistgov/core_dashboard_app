b3072b673eb090c191c708c2f9aa5315f1c744f1
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-10-02 11:25:34 -0400

    update version to 1.0.0-alpha4
    Change-Id: Idc18cc618d0b4dafafc11d07c0fd9b7fa2731b80

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_dashboard_app",
-    version="1.0.0-alpha3",
+    version="1.0.0-alpha4",
     description=("Core dashboard Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
