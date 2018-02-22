7d901c5603b5fdbabcfe65c6caed699083641e06
Adrien Catel <adrien.catel@nist.gov>
2018-02-22 09:52:56 -0500

    fix: rename menu items
    Change-Id: Ia5df899aed5d4107f1d9b409b8b95c3cd05f814f

--- core_dashboard_app/settings.py
+++ core_dashboard_app/settings.py
@@ -12,16 +12,16 @@ SERVER_URI = getattr(settings, 'SERVER_URI', "http://localhost")
 INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', [])
 
 menu = {
-    'My Templates': ('core_dashboard_templates', 1000),
-    'My Records': ('core_dashboard_records', 2000),
-    'My Files': ('core_dashboard_files', 3000),
-    'My Workspaces': ('core_dashboard_workspaces', 4000),
+    'Templates': ('core_dashboard_templates', 1000),
+    'Records': ('core_dashboard_records', 2000),
+    'Files': ('core_dashboard_files', 3000),
+    'Workspaces': ('core_dashboard_workspaces', 4000),
 }
 
 if 'core_composer_app' in INSTALLED_APPS:
-    menu['My Types'] = ('core_dashboard_types', 1500)
+    menu['Types'] = ('core_dashboard_types', 1500)
 if 'core_curate_app' in INSTALLED_APPS:
-    menu['My Forms'] = ('core_dashboard_forms', 2500)
+    menu['Forms'] = ('core_dashboard_forms', 2500)
 
 # Menu
 DASHBOARD_MENU = getattr(settings, 'DASHBOARD_MENU', menu)
