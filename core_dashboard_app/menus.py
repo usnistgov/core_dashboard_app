9741cbddb5effd430ebeda5e40a78303ae513bba
Adrien Catel <adrien.catel@nist.gov>
2018-05-24 14:25:44 -0400

    fix: custom labels implementation
    Change-Id: I39568568b169e0df50efe136901502b244e767ad

--- core_dashboard_app/menus.py
+++ core_dashboard_app/menus.py
@@ -4,32 +4,56 @@
 from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
-from core_dashboard_app.constants import FUNCTIONAL_OBJECT_ENUM
-from core_dashboard_app.settings import DASHBOARD_MENU, INSTALLED_APPS
+from core_dashboard_common_app.constants import FUNCTIONAL_OBJECT_ENUM
+from core_dashboard_app.settings import INSTALLED_APPS
 
-for item in DASHBOARD_MENU:
+
+Menu.add_item(
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()), reverse('core_dashboard_templates'))
+)
+
+Menu.add_item(
+    "dashboard", MenuItem(FUNCTIONAL_OBJECT_ENUM.RECORD.title(), reverse('core_dashboard_records'))
+)
+
+Menu.add_item(
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()), reverse('core_dashboard_files'))
+)
+
+Menu.add_item(
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()), reverse('core_dashboard_workspaces'))
+)
+
+sharing_children = (
+    MenuItem('All {0}'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.title()), reverse("admin:core_dashboard_records"),
+             icon="list"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()), reverse("admin:core_dashboard_templates"),
+             icon="list"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()), reverse("admin:core_dashboard_files"),
+             icon="list"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()), reverse("admin:core_dashboard_workspaces"),
+             icon="list"),
+)
+
+if 'core_composer_app' in INSTALLED_APPS:
     Menu.add_item(
-        "dashboard", MenuItem(item, reverse(DASHBOARD_MENU[item][0]), weight=DASHBOARD_MENU[item][1])
+        "dashboard",
+        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title()), reverse('core_dashboard_types'))
     )
+    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title()),
+                                  reverse("admin:core_dashboard_types"), icon="list"),)
+
+if 'core_curate_app' in INSTALLED_APPS:
+    Menu.add_item(
+        "dashboard",
+        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title()), reverse('core_dashboard_forms'))
+    )
+    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title()),
+                                  reverse("admin:core_dashboard_forms"), icon="list"),)
 
 Menu.add_item(
     "user", MenuItem("My Profile", reverse('core_dashboard_profile'))
 )
-
-sharing_children = (
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.title()), reverse("admin:core_dashboard_records"), icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()), reverse("admin:core_dashboard_templates"), icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()), reverse("admin:core_dashboard_files"), icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()), reverse("admin:core_dashboard_workspaces"), icon="list"),
-)
-
-if 'core_curate_app' in INSTALLED_APPS:
-    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title()),
-                                  reverse("admin:core_dashboard_forms"), icon="list"),)
-
-if 'core_composer_app' in INSTALLED_APPS:
-    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title()),
-                                  reverse("admin:core_dashboard_types"), icon="list"),)
 
 Menu.add_item(
     "admin", MenuItem("DASHBOARD", None, children=sharing_children)
--- core_dashboard_app/settings.py
+++ core_dashboard_app/settings.py
@@ -4,26 +4,9 @@
 
 from django.conf import settings
 
-from core_dashboard_app.constants import FUNCTIONAL_OBJECT_ENUM
-
 if not settings.configured:
     settings.configure()
 
 SERVER_URI = getattr(settings, 'SERVER_URI', "http://localhost")
 
 INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', [])
-
-menu = {
-    '{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()): ('core_dashboard_templates', 1000),
-    '{0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.title()): ('core_dashboard_records', 2000),
-    '{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()): ('core_dashboard_files', 3000),
-    '{0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()): ('core_dashboard_workspaces', 4000),
-}
-
-if 'core_composer_app' in INSTALLED_APPS:
-    menu['{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title())] = ('core_dashboard_types', 1500)
-if 'core_curate_app' in INSTALLED_APPS:
-    menu['{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title())] = ('core_dashboard_forms', 2500)
-
-# Menu
-DASHBOARD_MENU = getattr(settings, 'DASHBOARD_MENU', menu)
