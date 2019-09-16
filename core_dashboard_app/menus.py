3d8184097d8f3205c0038fc1379f0db667a1a36d
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-09-16 11:15:16 -0400

    refactor: enum
    Change-Id: Id448209dfe432c5aca99924e05666a188c454e8a

--- core_dashboard_app/menus.py
+++ core_dashboard_app/menus.py
@@ -8,46 +8,46 @@ from core_dashboard_app.settings import INSTALLED_APPS
 from core_dashboard_common_app.constants import FUNCTIONAL_OBJECT_ENUM
 
 Menu.add_item(
-    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()), reverse('core_dashboard_templates'))
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.value.title()), reverse('core_dashboard_templates'))
 )
 
 Menu.add_item(
-    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.title()), reverse('core_dashboard_records'))
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.value.title()), reverse('core_dashboard_records'))
 )
 
 Menu.add_item(
-    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()), reverse('core_dashboard_files'))
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.value.title()), reverse('core_dashboard_files'))
 )
 
 Menu.add_item(
-    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()), reverse('core_dashboard_workspaces'))
+    "dashboard", MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.value.title()), reverse('core_dashboard_workspaces'))
 )
 
 sharing_children = (
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.title()), reverse("admin:core_dashboard_records"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.RECORD.value.title()), reverse("admin:core_dashboard_records"),
              icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.title()), reverse("admin:core_dashboard_templates"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TEMPLATE.value.title()), reverse("admin:core_dashboard_templates"),
              icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.title()), reverse("admin:core_dashboard_files"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FILE.value.title()), reverse("admin:core_dashboard_files"),
              icon="list"),
-    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.title()), reverse("admin:core_dashboard_workspaces"),
+    MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.WORKSPACE.value.title()), reverse("admin:core_dashboard_workspaces"),
              icon="list"),
 )
 
 if 'core_composer_app' in INSTALLED_APPS:
     Menu.add_item(
         "dashboard",
-        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title()), reverse('core_dashboard_types'))
+        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.value.title()), reverse('core_dashboard_types'))
     )
-    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.title()),
+    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.TYPE.value.title()),
                                   reverse("admin:core_dashboard_types"), icon="list"),)
 
 if 'core_curate_app' in INSTALLED_APPS:
     Menu.add_item(
         "dashboard",
-        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title()), reverse('core_dashboard_forms'))
+        MenuItem('{0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.value.title()), reverse('core_dashboard_forms'))
     )
-    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.title()),
+    sharing_children += (MenuItem('All {0}s'.format(FUNCTIONAL_OBJECT_ENUM.FORM.value.title()),
                                   reverse("admin:core_dashboard_forms"), icon="list"),)
 
 Menu.add_item(
