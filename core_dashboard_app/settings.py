7ce58d93691370d3de5091c3969e034e1a7902b2
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:41:27 -0400

    docs: add settings doc
    Change-Id: Idc5cf4982aa0f6e8ed22f4048f229b230a9a7e81

--- core_dashboard_app/settings.py
+++ core_dashboard_app/settings.py
@@ -1,5 +1,7 @@
-"""
-    Django settings for core_dashboard_app app
+""" Django settings for core_dashboard_app app.
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 
 from django.conf import settings
