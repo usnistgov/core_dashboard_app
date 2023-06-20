ac38bab8ffdd6e824883595a8366b54670162593
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-06-20 15:49:26 -0400

    fix: remove tz_detect
    Change-Id: I3590b6495deff4b785c3715e9151ac4c734baf8b

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -32,7 +32,6 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "tz_detect.middleware.TimezoneMiddleware",
 )
 
 TEMPLATES = [
